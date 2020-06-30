27f9a2d0b7aa1c463bd91fa9940f2ccd02ef2626
sveilande@gmail.com <sveilande@gmail.com>
2020-06-30 15:10:52 +0300

    Version3

---
+++ Puzzle15/PuzzleBlock.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace Puzzle15
+{
+    class PuzzleBlock : Button 
+    {
+        public PuzzleBlock()
+        {
+            InitializePuzzleBlock()
+        }
+        private void InitializePuzzleBlock()
+        {
+            this.BackColor = Color.Orange;
+            this.Height = 80;
+            this.Width = 80;
+            this.Text = "0";
+            this.Font = new Font("Roboto", 18);
+            
+        }
+    }
+}

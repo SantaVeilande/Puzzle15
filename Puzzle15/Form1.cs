7217a2a8ab4cc870f8e7331b5f252c510a975c03
sveilande@gmail.com <sveilande@gmail.com>
2020-06-30 14:46:48 +0300

    Add project files.

---
+++ Puzzle15/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Puzzle15
+{
+    public partial class PuzzleArea : Form
+    {
+        public PuzzleArea()
+        {
+            InitializeComponent();
+            InitializePuzzleArea();
+            InitializeButtons();
+        }
+
+        private void InitializePuzzleArea()
+        {
+            this.BackColor = Color.Red;
+            this.Text = "Puzzle 15";
+        }
+
+        private void InitializeButtons()
+        {
+            Button button;
+            for(int i = 1; i < 16; i++)
+            {
+                button = new Button();
+                this.Controls.Add(button); 
+            }
+        }
+   
+    }
+}
